31f1d48b9bd7b524ec4c52e8af931648ddf3d20c
Kai <twnoken@gmail.com>
2019-06-03 13:55:55 +0800

    update all learning files

--- Hash_Table/Hashing.java
+++ Hash_Table/Hashing.java
@@ -1,105 +1,74 @@
-public class HashTable {
-    private static int INITIAL_SIZE = 16;
-    private HashEntry[] entries = new HashEntry[INITIAL_SIZE];
-    public void put(String key, String value) {
-        int hash = getHash(key);
-        final HashEntry hashEntry = new HashEntry(key, value);
-        if(entries[hash] == null) {
-            entries[hash] = hashEntry;
+public class Hashing {
+    static int INITIAL_SIZE = 16;
+    static HashItem[] items = new HashItem[INITIAL_SIZE];
+    static HashItem temp;
+
+    public static void main(String[] args) {
+        for(int i=0; i<30; i++) {
+            final String key = String.valueOf(i);
+            put(key, String.valueOf((int)(Integer.valueOf(key)+Math.random()*5000+1))) ;
         }
-        // If there is already an entry at current hash
-        // position, add entry to the linked list.
-        else {
-            HashEntry temp = entries[hash];
-            while(temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = hashEntry;
-        }
+
+        printT();
     }
 
-    /**
-     * Returns 'null' if the element is not found.
-     */
-    public String get(String key) {
-        int hash = getHash(key);
-        if(entries[hash] != null) {
-            HashEntry temp = entries[hash];
-
-            // Check the entry linked list for march
-            // for the given 'key'
-            while( !temp.key.equals(key)
-                    && temp.next != null ) {
-                temp = temp.next;
+    public static void put(String key, String value) {
+            int hash = getHash(key);
+            final HashItem hashItem = new HashItem(key, value);
+            if(items[hash] == null) {
+                items[hash] = hashItem;
             }
-            return temp.value;
+            // If there is already an item at current hash
+            // position, add item to the linked list.
+            else {
+                temp = items[hash];
+                while(temp.next != null) {
+                    temp = temp.next;
+                }
+                temp.next = hashItem;
+            }
         }
-
-        return null;
-    }
-
-    private int getHash(String key) {
+    
+    private static int getHash(String key) {
         // piggy backing on java string
         // hashcode implementation.
         return key.hashCode() % INITIAL_SIZE;
     }
 
-    public static class HashEntry {
-        String key;
-        String value;
-        // Linked list of same hash entries.
-        HashEntry next;
 
-        public HashEntry(String key, String value) {
-            this.key = key;
-            this.value = value;
-            this.next = null;
-        }
-
-        @Override
-        public String toString() {
-            return "[" + key + ", " + value + "]";
-        }
-    }
-
-    @Override
-    public String toString() {
-        int bucket = 0;
-        StringBuilder hashTableStr = new StringBuilder();
-        for (HashEntry entry : entries) {
-            if(entry == null) {
-                continue;
+    public static void printT(){
+        for (HashItem item : items) {
+            if(item == null) {
+                System.out.println("THIS ONE IS EMPTY!");
             }
-            hashTableStr.append("\n bucket[")
-                    .append(bucket)
-                    .append("] = ")
-                    .append(entry.toString());
-            bucket++;
-            HashEntry temp = entry.next;
-            while(temp != null) {
-                hashTableStr.append(" -> ");
-                hashTableStr.append(temp.toString());
-                temp = temp.next;
+            else{
+                temp = item;
+                do{
+                    System.out.print(temp.set+"-->");
+                    temp = temp.next;
+                } while(temp != null);
+                System.out.println("END");
             }
         }
-        return hashTableStr.toString();
+        
     }
 
-    public static void main(String[] args) {
-        HashTable hashTable = new HashTable();
-        // Put some key values.
-        for(int i=0; i<30; i++) {
-            final String key = String.valueOf(i);
-            hashTable.put(key, key);
+    public static class HashItem {
+            String key;
+            String value;
+            String set;
+            // Linked list of same hash items.
+            HashItem next;
+
+            public HashItem(String key, String value) {
+                this.set = "["+key+","+value+"]";
+                this.key = key;
+                this.value = value;
+                this.next = null;
+            }
         }
 
-        // Print the HashTable structure
-        log("****   HashTable  ***");
-        log(hashTable.toString());
-        log("\nValue for key(20) : " + hashTable.get("20") );
-    }
-
-    private static void log(String msg) {
-        System.out.println(msg);
-    }
+    
 }
+
+    
