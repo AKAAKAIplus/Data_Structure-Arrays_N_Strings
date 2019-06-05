242095e1b8e32f2975802fc7bb46cf08835d2730
Kai <twnoken@gmail.com>
2019-06-05 13:57:13 +0800

    uodate StringBuilder Implementation

---
+++ StringBuilder/StringBuilder_Implementation.java
@@ -0,0 +1,18 @@
+import java.lang.StringBuilder;
+
+public class StringBuilder_Implementation {
+	public static void main(String[] args) {
+    	//Utilize StringBuilder
+		StringBuilder sb = new StringBuilder();
+    	String[] words = {"Cheese", "Pepperoni", "Black Olives"};
+    	
+    	for(String word:words) {
+    		System.out.println("Appending:"+word);
+    		sb.append(word);
+    	}
+    	System.out.println(sb.toString());
+    	System.out.println("------");
+    	System.out.println(sb.indexOf("Pepperoni"));
+    	System.out.println(sb.reverse().toString());
+	}
+}
